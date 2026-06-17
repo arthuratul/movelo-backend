97fcb3021b58d69bc834ebc4ae66feb8b8bfb7f1
Arthur Atul <arthur.atul@rubicotech.in>
2026-06-17 12:21:52 +0530

    feat: add helmet middleware for secure HTTP headers

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
   const configService = app.get(ConfigService);
